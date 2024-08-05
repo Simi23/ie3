f47480bf5e021a94117f807fab6bd2db8ae18fb6
Simi23 <simitomi2003@gmail.com>
2024-08-05 20:02:56 +0200

    Add Session to DB schema

---
+++ prisma/migrations/20240805175821_add_session/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "Session" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL DEFAULT now() + '1 day'::interval,
+    "valid" BOOLEAN NOT NULL DEFAULT true,
+    "address" TEXT NOT NULL,
+    "userAgent" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Session" ADD CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
