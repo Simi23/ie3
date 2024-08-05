bf48de1ed3404bbff52cf2b068e429885e30cfe5
Simi23 <simitomi2003@gmail.com>
2024-08-05 21:58:14 +0200

    Add init page

---
+++ server/api/init/index.get.ts
@@ -0,0 +1,32 @@
+import { prisma } from "~/db/prismaClient";
+
+export default defineEventHandler(async (event) => {
+  const initDone = await prisma.option.findFirst({
+    where: {
+      name: "initDone",
+    },
+  });
+
+  if (initDone == null) {
+    await prisma.option.create({
+      data: {
+        name: "initDone",
+        value: false,
+      },
+    });
+
+    return false;
+  }
+
+  if (typeof initDone.value !== "boolean" || initDone.value === true) {
+    return true;
+
+    // throw createError({
+    //   statusCode: 406,
+    //   statusMessage: "Not Acceptable",
+    //   message: "Init already done",
+    // });
+  }
+
+  return false;
+});
