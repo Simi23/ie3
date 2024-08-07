7190a20f72f7b10875e4ab73c1e971d19cd71fa2
Simi <simitomi2003@gmail.com>
2024-08-07 23:17:50 +0200

    Simplify init page

--- pages/init.vue
+++ pages/init.vue
@@ -5,30 +5,10 @@
 </template>
 
 <script lang="ts" setup>
-const toast = useToast();
-
-const { data, error } = await useFetch("/api/init", {
-  onResponseError({ request, response, error }) {
-    toast.add({
-      title: "Hiba",
-      description: response?._data?.message ?? "Ismeretlen hiba",
-      icon: "i-heroicons-x-mark-20-solid",
-      color: "red",
-    });
-  },
-});
+const { data, error } = await useFetchNotification("/api/init");
 
 async function test() {
-  try {
-    const resp = await $fetch("/api/init");
-  } catch (e: any) {
-    toast.add({
-      title: "Hiba",
-      description: e.data.message ?? "Ismeretlen hiba",
-      icon: "i-heroicons-x-mark-20-solid",
-      color: "red",
-    });
-  }
+  const resp = await $fetchNotification("/api/init");
 }
 </script>
 
