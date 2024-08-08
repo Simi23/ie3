1da1d29143b7c20925eada0e56ec29a0807e4b2a
Simi <simitomi2003@gmail.com>
2024-08-08 19:47:46 +0200

    Rewrite custom useFetch to support types

--- utils/useFetchNotification.ts
+++ utils/useFetchNotification.ts
@@ -1,27 +1,40 @@
-import type { UseFetchOptions } from "nuxt/app";
-import { getLocale } from "~/assets/lang";
+import type { FetchError } from "ofetch";
+import type { AvailableRouterMethod, NitroFetchRequest } from "nitropack";
+import type { AsyncData, UseFetchOptions, FetchResult } from "nuxt/app";
 
-export default function useFetchNotification<T>(
-  url: string | (() => string),
-  options?: Partial<
-    Omit<UseFetchOptions<T>, "default"> & {
-      default: () => T | Ref<T>;
-    }
-  >,
-) {
-  const toast = useToast();
+type PickFrom<T, K extends Array<string>> =
+  T extends Array<any>
+    ? T
+    : T extends Record<string, any>
+      ? keyof T extends K[number]
+        ? T
+        : K[number] extends never
+          ? T
+          : Pick<T, K[number]>
+      : T;
+type KeysOf<T> = Array<
+  T extends T ? (keyof T extends string ? keyof T : never) : never
+>;
 
-  return useFetch(url, {
-    ...options,
-    onResponseError({ response }) {
-      toast.add({
-        title: getLocale("error"),
-        description: response?._data?.message
-          ? getLocale(response?._data?.message)
-          : getLocale("unknown-error"),
-        icon: "i-heroicons-x-mark-20-solid",
-        color: "red",
-      });
-    },
+export default function useFetchNotification<
+  ResT = void,
+  ErrorT = FetchError,
+  ReqT extends NitroFetchRequest = NitroFetchRequest,
+  Method extends AvailableRouterMethod<ReqT> = ResT extends void
+    ? "get" extends AvailableRouterMethod<ReqT>
+      ? "get"
+      : AvailableRouterMethod<ReqT>
+    : AvailableRouterMethod<ReqT>,
+  _ResT = ResT extends void ? FetchResult<ReqT, Method> : ResT,
+  DataT = _ResT,
+  PickKeys extends KeysOf<DataT> = KeysOf<DataT>,
+  DefaultT = null,
+>(
+  request: Ref<ReqT> | ReqT | (() => ReqT),
+  opts?: UseFetchOptions<_ResT, DataT, PickKeys, DefaultT, ReqT, Method>,
+): AsyncData<PickFrom<DataT, PickKeys> | DefaultT, ErrorT | null> {
+  return useFetch(request, {
+    ...opts,
+    $fetch: $fetchNotification,
   });
 }
