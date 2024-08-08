5a2313dbd2f788d7d11f46b884ce68731bc3fddb
Simi <simitomi2003@gmail.com>
2024-08-08 22:49:26 +0200

    Work on init page

--- pages/init.vue
+++ pages/init.vue
@@ -1,15 +1,99 @@
 <template>
   <div>
-    <UButton @click="test">Press me</UButton>
+    <UContainer class="flex min-h-screen w-fit items-center">
+      <UCard class="h-fit">
+        <template #header>
+          <h1 class="text-center text-2xl font-bold">Inicializáció</h1>
+          <h2 class="text-center text-lg font-semibold">
+            Adminisztrátori fiók létrehozása
+          </h2>
+        </template>
+
+        <div v-if="data === true">
+          A szerveren már megtörtént az inicializáció.
+        </div>
+        <div v-else>
+          <UForm
+            :schema="initSchema"
+            :state="initState"
+            @submit="initSubmit"
+            @error="initError"
+          >
+            <UFormGroup
+              name="username"
+              label="Felhasználónév"
+              class="mx-1 my-2 h-20"
+            >
+              <UInput v-model="initState.username" class="inputField"></UInput>
+            </UFormGroup>
+            <UFormGroup name="email" label="Email cím" class="mx-1 my-2 h-20">
+              <UInput v-model="initState.email" class="inputField"></UInput>
+            </UFormGroup>
+            <UFormGroup name="password" label="Jelszó" class="mx-1 my-2 h-20">
+              <UInput
+                v-model="initState.password"
+                type="password"
+                class="inputField"
+              ></UInput>
+            </UFormGroup>
+            <UFormGroup
+              name="confirmPassword"
+              label="Jelszó ismét"
+              class="mx-1 my-2 h-20"
+            >
+              <UInput
+                v-model="initState.confirmPassword"
+                type="password"
+                class="inputField"
+              ></UInput>
+            </UFormGroup>
+
+            <UButton type="submit" class="mx-auto mt-5 block"
+              >Regisztráció</UButton
+            >
+          </UForm>
+        </div>
+      </UCard>
+    </UContainer>
   </div>
 </template>
 
 <script lang="ts" setup>
-const { data, error } = await useFetchNotification("/api/init");
+import { z } from "zod";
+import type { FormSubmitEvent, FormErrorEvent } from "#ui/types";
 
-async function test() {
-  const resp = await $fetchNotification("/api/init");
+const { data } = await useFetchNotification("/api/init");
+
+const initSchema = z
+  .object({
+    username: z.string().min(4, "Legalább 4 karakternek kell lennie!"),
+    email: z.string().email("Helyes email cím megadása kötelező!"),
+    password: z.string().min(8, "Legalább 8 karakternek kell lennie!"),
+    confirmPassword: z.string().min(8, "Legalább 8 karakternek kell lennie!"),
+  })
+  .refine((data) => data.password == data.confirmPassword, {
+    message: "Nem egyeznek a jelszavak!",
+    path: ["confirmPassword"],
+  });
+type InitSchema = z.output<typeof initSchema>;
+
+const initState = ref({
+  username: undefined,
+  email: undefined,
+  password: undefined,
+  confirmPassword: undefined,
+});
+
+function initSubmit(event: FormSubmitEvent<InitSchema>) {
+  console.log("Hello world");
 }
+
+function initError(event: FormErrorEvent) {}
 </script>
 
-<style></style>
+<style>
+.inputField ~ p {
+  user-select: none;
+  margin-top: 0;
+}
+</style>
