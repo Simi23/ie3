2b302af406647f1b2b6f104a2cc0903c42cda2ab
Simi <simitomi2003@gmail.com>
2024-08-09 23:12:21 +0200

    Start implementing carousel menu

---
+++ components/CarouselMenu.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <slot v-for="i in pagecount" :key="i" :name="'page' + i"></slot>
+  </div>
+</template>
+
+<script lang="ts" setup>
+interface Props {
+  pagecount?: number;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  pagecount: 1,
+});
+</script>
+
+<style></style>
