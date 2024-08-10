9d362d340347c70c445edaec49a7d4daf351dcb5
Simi <simitomi2003@gmail.com>
2024-08-10 21:00:15 +0200

    Complete CarouselMenu component and create test page

--- components/CarouselMenu.vue
+++ components/CarouselMenu.vue
@@ -1,17 +1,97 @@
 <template>
   <div>
-    <slot v-for="i in pagecount" :key="i" :name="'page' + i"></slot>
+    <div
+      class="size-transition relative contain-content"
+      :style="{ height: boxHeight }"
+    >
+      <div
+        v-for="i in props.pagecount"
+        :key="i"
+        :id="`page${i}`"
+        class="scrolled-page absolute left-0 top-0 h-fit w-full"
+      >
+        <slot :name="'page' + i"></slot>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 interface Props {
   pagecount?: number;
+  initialheight?: string;
 }
 
 const props = withDefaults(defineProps<Props>(), {
   pagecount: 1,
+  initialheight: "300px",
+});
+
+const boxHeight = ref<string>(props.initialheight);
+
+defineExpose({
+  jumpTo,
+});
+
+let curPageNum = 1;
+
+async function jumpTo(number: number) {
+  if (number == curPageNum) return;
+
+  const previousElement = document.getElementById(`page${curPageNum}`);
+  const nextElement = document.getElementById(`page${number}`);
+
+  if (previousElement === null || nextElement === null) return;
+
+  let forwardX = "";
+  let backwardX = "";
+
+  if (number > curPageNum) {
+    // Scroll forwards
+    forwardX = "translateX(100%)";
+    backwardX = "translateX(-100%)";
+  } else {
+    //Scroll backwards
+    forwardX = "translateX(-100%)";
+    backwardX = "translateX(100%)";
+  }
+
+  boxHeight.value = nextElement.clientHeight + "px";
+
+  previousElement.style.transform = "translateX(0)";
+  previousElement.style.opacity = "1";
+  nextElement.style.transition = "none";
+  nextElement.style.transform = forwardX;
+  nextElement.style.opacity = "0";
+  await sleep(20);
+  previousElement.style.transition = "opacity 0.2s, transform 0.4s ease-in";
+  nextElement.style.transition = "opacity 0.2s, transform 0.4s ease-in-out";
+  nextElement.style.visibility = "visible";
+  await sleep(20);
+  previousElement.style.transform = backwardX;
+  previousElement.style.opacity = "0";
+  nextElement.style.transform = "translateX(0)";
+  nextElement.style.opacity = "1";
+  await sleep(450);
+  previousElement.style.visibility = "hidden";
+
+  curPageNum = number;
+}
+
+onMounted(() => {
+  const currentElement = document.getElementById(`page${curPageNum}`);
+  if (currentElement === null) return;
+  boxHeight.value = currentElement.clientHeight + "px";
+  currentElement.style.visibility = "visible";
 });
 </script>
 
-<style></style>
+<style scoped>
+.size-transition {
+  transition: height 0.4s ease-in-out;
+}
+
+.scrolled-page {
+  visibility: hidden;
+}
+</style>
---
+++ pages/test.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <UContainer class="flex min-h-screen w-fit items-center">
+      <UCard class="h-fit">
+        <template #header>
+          <h1 class="text-center text-2xl font-bold">Inicializáció</h1>
+          <h2 class="text-center text-lg font-semibold">
+            Adminisztrátori fiók létrehozása
+          </h2>
+        </template>
+        <UButton @click="carouselmenu?.jumpTo(1)">1</UButton>
+        <UButton @click="carouselmenu?.jumpTo(2)">2</UButton>
+        <UButton @click="carouselmenu?.jumpTo(3)">3</UButton>
+        <CarouselMenu :pagecount="3" ref="carouselmenu" initialheight="10rem">
+          <template #page1>
+            <div class="h-40 w-full dark:bg-blue-400"></div>
+          </template>
+          <template #page2>
+            <div class="h-80 w-full dark:bg-yellow-400"></div>
+          </template>
+          <template #page3>
+            <div class="h-60 w-full dark:bg-emerald-400"></div>
+          </template>
+        </CarouselMenu>
+      </UCard>
+    </UContainer>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import type CarouselMenu from "~/components/CarouselMenu.vue";
+const carouselmenu = ref<InstanceType<typeof CarouselMenu> | null>(null);
+
+const loadingSpinner = useLoadingSpinner();
+const toast = useToast();
+</script>
+
+<style></style>
