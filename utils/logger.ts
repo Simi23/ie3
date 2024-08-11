b620feb84d834cc4b8e8703aead30e0a0b376268
Simi <simitomi2003@gmail.com>
2024-08-12 00:07:26 +0200

    Create logger utility

---
+++ utils/logger.ts
@@ -0,0 +1,31 @@
+import { prisma } from "~/db/prismaClient";
+import type { H3Event, EventHandlerRequest } from "h3";
+
+interface LogParameters {
+  clientAddress: string;
+  clientUserAgent: string;
+
+  severity: "INFO" | "WARN" | "ERROR";
+
+  category: "INIT" | "AUTH" | "REGISTRATION" | "COMPETITION";
+  message: string;
+}
+
+export async function logAction(params: LogParameters) {
+  await prisma.auditLog.create({
+    data: {
+      ...params,
+    },
+  });
+}
+
+export async function logEventAction(
+  event: H3Event<EventHandlerRequest>,
+  params: Omit<LogParameters, "clientAddress" | "clientUserAgent">,
+) {
+  await logAction({
+    clientAddress: event.context.realIp,
+    clientUserAgent: event.context.userAgent,
+    ...params,
+  });
+}
