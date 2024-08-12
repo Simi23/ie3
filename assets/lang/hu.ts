f54b007f998a73f13e22c213cb578039b256ba35
Simi <simitomi2003@gmail.com>
2024-08-12 22:37:15 +0200

    Expand locale

--- assets/lang/hu.ts
+++ assets/lang/hu.ts
@@ -8,6 +8,10 @@ const strings = {
   "unknown-error": "Ismeretlen hiba",
   error: "Hiba",
   "init-already-done": "Már megtörtént az inicializáció",
+  "form-invalid": "Az űrlap kitöltése helytelen!",
+  success: "Siker",
+  warning: "Figyelmeztetés",
+  information: "Információ",
 };
 
 export const huLang = new Proxy(strings, handler);
