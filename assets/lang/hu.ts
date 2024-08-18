60a362e58e0fc9172f2dbba9e96bb57cefa7d94b
Simi <simitomi2003@gmail.com>
2024-08-19 00:07:48 +0200

    Update locale

--- assets/lang/hu.ts
+++ assets/lang/hu.ts
@@ -16,6 +16,8 @@ const strings = {
     "Ezzel a felhasználónév/jelszó kombinációval nem található fiók!",
   "email-not-verified": "A megadott email cím még nincs megerősítve!",
   "session-not-found": "Érvénytelen belépés! Kérlek, jelentkezz be!",
+  "access-denied": "Hozzáférés megtagadva!",
+  "validation-failed": "A megadott adatok helytelenek!s",
 };
 
 export const huLang = new Proxy(strings, handler);
