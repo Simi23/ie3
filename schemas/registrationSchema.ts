cb4008677fb7af060f66fb642211d81d0c5d56b4
Simi <simitomi2003@gmail.com>
2024-12-30 10:38:53 +0100

    Split registration schema

--- schemas/registrationSchema.ts
+++ schemas/registrationSchema.ts
@@ -2,16 +2,16 @@ import { z } from "zod";
 
 export const registrationSchema = z
   .object({
-    fullname: z.string().min(1, "Adja meg a teljes nevét!"),
+    fullname: z.string().min(1, "Add meg a teljes nevedet!"),
     username: z.string().min(4, "Legalább 4 karakternek kell lennie!"),
     email: z.string().email("Helyes email cím megadása kötelező!"),
     password: z.string().min(8, "Legalább 8 karakternek kell lennie!"),
     confirmPassword: z.string().min(8, "Legalább 8 karakternek kell lennie!"),
-    classId: z.string().min(1, "Válassza ki az osztályát!"),
+    classId: z.string().min(1, "Válaszd ki az osztályodat!"),
     ownPc: z.boolean(),
     ethernetPort: z.boolean(),
     ownChair: z.boolean(),
-    seatName: z.string().min(1, "Válasszon ki egy ülőhelyet!"),
+    seatName: z.string().min(1, "Válassz ki egy ülőhelyet!"),
   })
   .refine((data) => data.password == data.confirmPassword, {
     message: "Nem egyeznek a jelszavak!",
@@ -19,3 +19,46 @@ export const registrationSchema = z
   });
 
 export type RegistrationSchema = z.output<typeof registrationSchema>;
+
+// 1/3
+
+export const registrationSchema1p3 = z
+  .object({
+    email: z.string().email("Helyes email cím megadása kötelező!"),
+    username: z.string().min(4, "Legalább 4 karakternek kell lennie!"),
+    password: z.string().min(8, "Legalább 8 karakternek kell lennie!"),
+    confirmPassword: z.string().min(8, "Legalább 8 karakternek kell lennie!"),
+  })
+  .refine((data) => data.password == data.confirmPassword, {
+    message: "Nem egyeznek a jelszavak!",
+    path: ["confirmPassword"],
+  });
+
+export type RegistrationSchema1p3 = z.output<typeof registrationSchema1p3>;
+
+// 2/3
+
+export const registrationSchema2p3 = z.object({
+  fullname: z.string().min(1, "Add meg a teljes nevedet!"),
+  classId: z.string().min(1, "Válaszd ki az osztályodat!"),
+});
+
+export type RegistrationSchema2p3 = z.output<typeof registrationSchema2p3>;
+
+// 3/3
+
+export const registrationSchema3p3 = z.object({
+  ownPc: z.boolean(),
+  ethernetPort: z.boolean(),
+  ownChair: z.boolean(),
+});
+
+export type RegistrationSchema3p3 = z.output<typeof registrationSchema3p3>;
+
+// SEAT
+
+export const registrationSchemaSeat = z.object({
+  seatName: z.string().min(1, "Válassz ki egy ülőhelyet!"),
+});
+
+export type RegistrationSchemaSeat = z.output<typeof registrationSchemaSeat>;
