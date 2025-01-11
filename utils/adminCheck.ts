a8654b3ee30090ac70f21c5e5976b7ab74857fe5
Simi <simitomi2003@gmail.com>
2025-01-11 17:30:33 +0100

    Log unauthorized requests

--- utils/adminCheck.ts
+++ utils/adminCheck.ts
@@ -4,7 +4,25 @@ export default function (
   event: H3Event<EventHandlerRequest>,
   minLevel: number,
 ) {
-  if (Number(event.context.user?.adminClass ?? 0) < minLevel) {
+  if (event.context.user === undefined) {
+    logEventAction(event, {
+      category: "AUTH",
+      severity: "WARN",
+      message: `Client ${event.context.clientAddress ?? "unknown-ip"} tried to access endpoint ${event.path}`,
+    });
+    throw createError({
+      statusCode: 403,
+      statusMessage: "Forbidden",
+      message: "access-denied",
+    });
+  }
+
+  if (Number(event.context.user.adminClass ?? 0) < minLevel) {
+    logEventAction(event, {
+      category: "AUTH",
+      severity: "WARN",
+      message: `Unauthenticated user ${event.context.user.username} tried to access endpoint ${event.path}`,
+    });
     throw createError({
       statusCode: 403,
       statusMessage: "Forbidden",
