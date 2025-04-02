15b880a387fd23c00a6f85f3f0a15255b8e386b1
Simi <simitomi2003@gmail.com>
2025-04-02 22:19:12 +0200

    Create Discord user badge

---
+++ components/DiscordUserBadge.vue
@@ -0,0 +1,24 @@
+<template>
+  <UBadge :label="props.dcData.globalName" size="md" variant="subtle">
+    <template #leading>
+      <UAvatar :src="dcAvatar" size="3xs" crossorigin />
+    </template>
+  </UBadge>
+</template>
+
+<script lang="ts" setup>
+type Props = {
+  dcData: {
+    avatar: string;
+    id: string;
+    globalName: string;
+  };
+};
+const props = defineProps<Props>();
+
+const dcAvatar = computed(() => {
+  return `https://cdn.discordapp.com/avatars/${props.dcData.id}/${props.dcData.avatar}.jpg?size=128`;
+});
+</script>
+
+<style></style>
